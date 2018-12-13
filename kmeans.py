90e55ac23627430a57175522dcfc3326f8761e82
Tofunmi <jayteae94@gmail.com>
2018-12-13 08:03:04 +0100

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -23,48 +23,30 @@ def kmeans(X, k, max_iter=100):
     Returns:
         centers: A matrix of the computed cluster centers of shape [k, num_features]
         assign: A vector of cluster assignments for each example in X of shape [num_train] n
-
     """
 
     centers = None
     assign = None
 
-    start = time.time()
-
-
-    #######################################################################
-    # TODO:                                                               #
-    # Perfom k-means clustering of the input data X and store the         #
-    # resulting cluster-centers as well as cluster assignments.           #
-    #                                                                     #
-    #######################################################################
+    start = time.time()    
     
-    # 1st step: Chose k random rows of X as initial cluster centers        
-    
-    #centers = X[np.random.randint(X.shape[0], size=k), :]        
+    # 1st step: Chose k random rows of X as initial cluster centers               
     centers = X[np.random.choice(np.arange(len(X)), k), :]    
     distances = np.zeros([X.shape[0], k], dtype=np.float64)
     
     for i in range(max_iter):
         prev_assign = assign
         
-        # 2nd step: Update the cluster assignment
-        
-        #assign = np.array([np.argmin([np.dot(x_i-y_k, x_i-y_k) for y_k in centers]) for x_i in X]) 
+        # 2nd step: Update the cluster assignment                
         for i, c in enumerate(centers):
             distances[:, i] = get_distances(c, X)
         
         # 3rd step: Check for convergence        
             assign = np.argmin(distances, axis=1)
         
-        # 4th step: Update the cluster centers based on the new assignment                
-        
+        # 4th step: Update the cluster centers based on the new assignment                        
         for c in range(k):
-                centers[c] = np.mean(X[assign == c], 0)
-
-    #######################################################################
-    #                         END OF YOUR CODE                            #
-    #######################################################################
+                centers[c] = np.mean(X[assign == c], 0)    
     
     exec_time = time.time()-start
     print('Number of iterations: {}, Execution time: {}s'.format(i+1, exec_time))
