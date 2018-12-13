43512f42131d9a8dcd374bd31cbf36b60bed78d8
Tofunmi <jayteae94@gmail.com>
2018-12-13 08:01:41 +0100

    Update getEigenImages.py

--- getEigenImages.py
+++ getEigenImages.py
@@ -1,21 +1,11 @@
 import numpy as np
 
-
 def getEigenImages(images, basis):
                    
     reconstruction = None           
     k, i = basis.shape
     u, v = images.shape
-    #######################################################################
-    # TODO:                                                               #
-    #      Compute eigen coefficients and reconstruct the faces from      #
-    #      coefficients                                                   #
-    # Input:  images - images to compress                                 #
-    #         basis - eigenbasis for compression                          #
-    # Output: eigen_coefficients - coefficients corresponding to each     #
-    #                              eigenvector                            #
-    #         reconstruction - compressed images                          #
-    #######################################################################                                          
+                                              
     eigen_coefficients = []    
     
     for i in range(u):
@@ -26,9 +16,6 @@ def getEigenImages(images, basis):
     for j in range(u):        
         var.append(np.dot(eigen_coefficients[j].T, basis.T))        
     
-    reconstruction = (var)            
-    #######################################################################
-    #                         END OF YOUR CODE                            #
-    #######################################################################
+    reconstruction = (var)                
 
     return eigen_coefficients, reconstruction
